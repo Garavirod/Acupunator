f0267be9a2338a15e7e8da7f11671d5c5362e1e1
Garavirod <rodrigogarciaavila26@gmail.com>
2020-10-14 22:27:42 -0500

    Acupunator file

--- acupunator-desktop/setup.py
+++ acupunator-desktop/setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 #LAS LIBRERIAS SE INSTALAN CON OTRO.PY
 include_files = [
-        'main.py',
+        'Acupunator.py',
         'requirements.txt',
         'setup.py',
         'templates/','img/','packages/']
@@ -10,4 +10,4 @@ setup( name = "Acupunator", version = "1.0",
        description = "Sistema de realidad virtual para estudiar acupuntura",
        options = {'build_exe': {'include_files':include_files}},
        author = 'Rodrigo García Ávila, Luis Enrique Hernández Tapia',
-       executables = [Executable("main.py",base = "Win32GUI", icon="acupunator.ico")])
+       executables = [Executable("Acupunator.py",base = "Win32GUI", icon="acupunator.ico")])
