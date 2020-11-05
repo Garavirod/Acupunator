6a7bc94c1abce2446939558d2c6cebe500b0caca
Garavirod <rodrigogarciaavila26@gmail.com>
2020-11-04 21:59:31 -0600

    Se agregó la calse de deserialización del JSON

--- acupunator-desktop/packages/simulator/DeserializationData.cs
+++ acupunator-desktop/packages/simulator/DeserializationData.cs
@@ -10,11 +10,16 @@ namespace Acupunator
         static void Main(string[] args)
         {
             string data = getDataJson();
-            //Console.WriteLine(data);
-            DeserializeData(data);
-            
+            //Deserializamos el JSON y obtenemos un obj deserializado (SÓLO EL CANL DEL PULMÓN)
+            Chanel ObjjDeserialized = DeserializeData(data); 
+            //Impimirmos algunos elementos del Obj canal 
+            Console.WriteLine(string.Format("Canal:> {0} Punto 1:>  {1} Alias del punto 1:> {2}", 
+                ObjjDeserialized.Name, ObjjDeserialized.Points[0].Name, ObjjDeserialized.Points[0].Alias));
         }
 
+        /*
+         * Éste método se encarga de parsear un arichivo JSON a una cadena de texto
+        */
         public static string getDataJson()
         {
             string data;
@@ -25,12 +30,13 @@ namespace Acupunator
 
             return data;
         }
-
-        private static void DeserializeData(string data)
-        {
-            
+        /*
+         * Este método deserializa una cadena de texto con formato JSON y lo mapea a clases previaente definidas
+        */
+        private static Chanel DeserializeData(string data)
+        {            
             var dataDeserialized = JsonConvert.DeserializeObject<List<Chanel>>(data);
-            Console.WriteLine(string.Format("Canal {0} punto 1 {1}", dataDeserialized[0].Name, dataDeserialized[0].Points[0].Name));
+            return dataDeserialized[0]; //Canal del pulmón
         }
     }
 }
